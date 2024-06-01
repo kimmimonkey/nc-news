194631e90419475fe9cc69694849a7f2022f8520
kimmimonkey <kim_muncey@hotmail.com>
2024-06-01 13:51:25 +0100

    completes error handling testing and logic for DELETE /api/comments/:comment_id endpoint

--- models/appModel.js
+++ models/appModel.js
@@ -116,17 +116,17 @@ exports.updateArticleVotes = (article_id, inc_votes) => {
 }
 
 exports.removeCommentById = (comment_id) => {
-    // return db
-    //     .query(`SELECT * from comments WHERE comment_id = $1`, [comment_id])
-    //     .then(({ rows: comments }) => {
-    //         if (comments.length === 0) {
-    //             return Promise.reject({
-    //                 status: 404,
-    //                 msg: "Not found"
-    //             })
-    //         }
-    //     })
-    // .then(() =>
     return db
-    .query(`DELETE FROM comments WHERE comment_id = $1;`, [comment_id])
+        .query(`SELECT * from comments WHERE comment_id = $1`, [comment_id])
+        .then(({ rows: comments }) => {
+            if (comments.length === 0) {
+                return Promise.reject({
+                    status: 404,
+                    msg: "Not found"
+                })
+            }
+        })
+    .then(() =>{
+    return db
+    .query(`DELETE FROM comments WHERE comment_id = $1;`, [comment_id])})
 }
